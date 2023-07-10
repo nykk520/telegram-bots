e11644af7b26ed268d704dee3396a5c412854418
178251115 <117524321+nykk520@users.noreply.github.com>
2023-07-10 23:21:24 +0800

    Update set.php

--- set.php
+++ set.php
@@ -5,8 +5,8 @@ namespace app;
 use Telegram\Bot\Api;
 
 
-$telegram = new Api('6299876389:AAF8QSEztrGOUZ5WDvhNjyyLiOd_dKyUt0E');
-$response = $telegram->setWebhook(['url' => 'https://earn.fengfupay.vip/admin/telegram/webhook']);
+$telegram = new Api('6299876389:888');
+$response = $telegram->setWebhook(['url' => 'https://yourdomain/admin/telegram/webhook']);
 if (!$response) {
     file_put_contents('/tmp/tglog', '启动tg机器人失败！');
 } else {
